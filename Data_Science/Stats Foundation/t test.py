e83cd4ab7c312cffb83fc21bdb9dd58acc882641
Phillip Hale <phhale@microsoft.com>
2018-11-22 05:18:11 -0800

    mod 2

--- Data_Science/Stats Foundation/t test.py
+++ Data_Science/Stats Foundation/t test.py
@@ -1,7 +1,9 @@
+#%% [markdown]
 # Basic Example for performing a T Test
 ## .. source: Statistisc and Machine Learning in Python
 ## Scenario: Given the following samples, test whether the true mean is 1.75
 
+#%%
 import numpy as np
 import scipy.stats as stats
 import matplotlib.pyplot as plt
@@ -16,9 +18,17 @@ n = len(x)              # sample size
 tobs = (xbar - mu0) / (s / np.sqrt(n))      # t-statistic
 print(tobs)
 
+#%% [markdown]
 # calculate the t-value
+#%%
 tvalues = np.linspace(-10, 10, 100)
 plt.plot(tvalues, stats.t.pdf(tvalues, n-1), 'b-', label="T(n-1)")
 upper_tval_tvalues = tvalues[tvalues > tobs]
 plt.fill_between(upper_tval_tvalues, 0, stats.t.pdf(upper_tval_tvalues, n-1), alpha = .9, label="p-value")
 _ = plt.legend()
+
+
+#%%
+
+
+#%%
